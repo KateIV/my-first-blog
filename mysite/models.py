d3f6eb6b809500dc37951e3c0b50a778ea53da42
KateIV <ivanovaekaterinadm@gmail.com>
2022-06-10 18:07:47 +0300

    My Django app, first commit

---
+++ mysite/models.py
@@ -0,0 +1,19 @@
+#from django.db import models
+
+#class Reporter(models.Model):
+    #full_name = models.CharField(max_length=70)
+
+    #def __str__(self):
+#   return self.full_name
+
+#class Article(models.Model):
+   # pub_date = models.DateField()
+  #  headline = models.CharField(max_length=200)
+   # content = models.TextField()
+  #  reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+
+if __name__ == '__models__':
+    print(1);
+    for i in range(1, 6):
+        print(i)
